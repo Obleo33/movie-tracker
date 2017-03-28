b35c88757bff97ae0e5aafd98c7dbce2348185e4
Franklin Crosby <obleo33@gmail.com>
2017-03-28 10:55:22 -0600

    first

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -8,7 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Movie Watcher</h1>
+        <h1>Movie Watcher v1</h1>
         {this.props.children}
       </div>
     )
