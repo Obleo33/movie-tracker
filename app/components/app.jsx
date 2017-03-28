c8eed5e64dda7908b5ecb1f5baf332ba6ceb993d
Franklin Crosby <obleo33@gmail.com>
2017-03-28 17:43:58 -0600

    Initial redux setup

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+// import { Route } from 'react-router-dom'
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+// import { Route } from 'react-router-dom';
+// import CreateItemContainer from './createItemContainer.js';
+// import { NavBar }from './navBar.js';
+// import { Home }from './home.js';
+// import ListItemContainer from './listItemContainer.js';
 
 export default class App extends Component {
   constructor() {
