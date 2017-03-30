9011e7bdb4106c5807915d26e8cf71da803f6962
Staci McWilliams <stacinicole86@gmail.com>
2017-03-30 12:41:25 -0600

    some styling

--- app/App/App.js
+++ app/App/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Movie Watcher v1</h1>
+        <h1 className="movie-header">MY FLICKSTER BOOK</h1>
         <Route exact path='/' component={NowShowingContainer}/>
       </div>
     )
--- app/NowShowing/NowShowingContainer.js
+++ app/NowShowing/NowShowingContainer.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux'
 import * as actions from '../actions'
 import NowShowing from './NowShowing'
 
+
 const mapStateToProps = state => state
 
 const mapDispatchToProps = dispatch => {
