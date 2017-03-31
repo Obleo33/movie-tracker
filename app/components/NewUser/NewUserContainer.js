bafe14e9e4b4deeeda32c63b3375ec5a399f672f
Staci McWilliams <stacinicole86@gmail.com>
2017-03-30 21:55:29 -0600

    add pseudo code for newUser component

---
+++ app/components/NewUser/NewUserContainer.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux'
+import newUser from '../NewUser/NewUser.js'
+import * as actions from '../actions'
+
+const mapStateToProps = (state) => {
+  return {
+    state
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(newUser)
