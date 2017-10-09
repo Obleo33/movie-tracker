f92fdf5760c31b6e31463ddeb639b94acd2d35c2
Franklin Crosby <obleo33@gmail.com>
2017-10-09 11:57:57 -0600

    Update webpack.prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ module.exports = {
    output: {
      path: path.join(__dirname, 'app'),
      filename: 'bundle.js',
-     publicPath: '/app/'
+     publicPath: '/'
    },
    module: {
      loaders: [{
