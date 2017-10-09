f3dd49ad89697ab74fc25f4adcf19048eff3aa7a
Franklin Crosby <obleo33@gmail.com>
2017-10-09 13:36:30 -0600

    Another try

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV !== 'production') {
   }));
 }
 
-app.use(express.static('/'));
+app.use(express.static('app'));
 
 app.get('/', function (req, res) { res.sendFile(path.join(__dirname, './index.html')) });
 
