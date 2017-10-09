de3de1c87ae0427697fd59fc6be705b3f0776d5f
Franklin Crosby <obleo33@gmail.com>
2017-10-09 12:57:57 -0600

    Adjust webpack.prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,29 +4,29 @@ const webpack = require('webpack');
 module.exports = {
   devtool: '#source-map',
   context: __dirname,
-   entry: [
-     './app/index.js'
-   ],
-   output: {
-     path: path.join(__dirname, 'app'),
-     filename: 'bundle.js',
-     publicPath: '/app/'
-   },
-   module: {
-     loaders: [
-     {
-       test: /.jsx?$/,
-       loader: 'babel-loader',
-       include: path.join(__dirname, 'app'),
-       exclude: /node_modules/,
-       query: {
-         presets: ['es2015', 'react', 'stage-3']
-       }
-     },
-   { test: /\.css$/, loader: 'style!css' },
-   { test: /\.svg$/, loader: 'svg-url-loader' }
- ]
-   },
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'app'),
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'app'),
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react', 'stage-3']
+        }
+      },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!sass' }
+    ]
+  },
   resolve: {
     extensions: ['', '.js', '.jsx', '.json', '.scss', '.css']
   }
